52840498ab4d36c3ba0cbb5a52508fed2167145c
Fazil S <fazil.s@experionglobal.local>
2023-11-03 09:52:24 +0530

    re arranged and added task3

--- day2/02_day2_Task02/js/main.js
+++ day2/02_day2_Task02/js/main.js
@@ -2,9 +2,37 @@
 
 const number = [1, 2, 3, 4, 5];
 
-const sum = (args) => {
-  console.log(args);
+// const sum = (args) => {
+//   console.log(args);
 
-  console.log(typeof args);
+//   console.log(typeof args);
+// };
+// sum(number);
+
+// const sum = (args) => {
+//   let sum = 0;
+//   for (let i = 0; i < args.length; i++) {
+//     sum += arg[i];
+//   }
+//   console.log(sum);
+// };
+
+// sum(number);
+
+//  maps
+
+const sum1 = (args) => {
+  let sum = 0;
+  args.map((element) => (sum += element));
+  console.log(sum);
 };
-sum(number);
+sum1(number);
+
+
+
+const sum2 = (args) => {
+  let sum = 0;
+  args.forEach((element)=>sum+=element);
+  console.log(sum);
+};
+sum2(number);
---
+++ day2/02_day2_Task03/js/main.js
@@ -0,0 +1,8 @@
+// sort
+
+console.log(["A", "Z", "P", "Q", "B"].sort());
+
+// sorting number
+// use sort((a,b)=>a-b)  this while sorting number because of the default sorting fuction
+
+console.log([1, 42, 12, 53, 532, 145, 11, 10].sort((a, b) => a - b));
