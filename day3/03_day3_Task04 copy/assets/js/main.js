9d466c1f1a7bfb9ad27fb81acaf0a6090c139a27
Fazil S <fazil.s@experionglobal.local>
2023-11-06 12:09:41 +0530

    added day 3

--- day3/03_day3_Task04 copy/assets/js/main.js
+++ day3/03_day3_Task04 copy/assets/js/main.js
@@ -1,11 +1,24 @@
+// let countValue = new Promise((resolve,  )=>{
+//   resolve("Promise resolved");
+// });
+
+// const v1 = (res)=>{
+//   console.log(res);
+// }
+// const v2 = () => {
+//   console.log("you can call multiple functions this way ");
+
+// }
 let countValue = new Promise((resolve,  )=>{
   resolve("Promise resolved");
 });
 
 const v1 = (res)=>{
   console.log(res);
+  return res
 }
-const v2 = () => {
+const v2 = (data) => {
+  console.log(data + ` v2 data print`);
   console.log("you can call multiple functions this way ");
 
 }
